b70183c002eacf4495e9c1c264d87badbeb3b402
raisaugat <rai.saugat.sr@gmail.com>
2020-05-16 16:11:56 +0545

    remove contact in home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,9 +10,6 @@ const IndexPage = () => {
       <Head title="Home" />
       <h1>Hello</h1>
       <h2>Saugat Rai</h2>
-      <p>
-        Need a developer? <Link to="/contact">Contact Me</Link>
-      </p>
     </Layout>
   )
 }
